ed1cc8886dc4c839737c22593565d1abd69cebcb
Anurag Sonawane <anurag50434411@gmail.com>
2026-04-01 22:58:52 +0530

    fetch now working

---
+++ script.js
@@ -0,0 +1,60 @@
+let con = document.getElementById('container')
+let cart = document.getElementById('cart')
+let cartItems = []
+
+console.log('Container:', con)
+console.log('Cart:', cart)
+
+fetch('https://dummyjson.com/products')
+    .then(response => {
+        console.log('Response:', response)
+        return response.json()
+    })
+    .then(data => {
+        console.log('Products:', data.products)
+        data.products.forEach((res) => {
+
+            let card = document.createElement('div')
+            const img = document.createElement('img')
+            img.src = res.image
+            img.alt = res.title
+            const title = document.createElement('h3')
+            title.textContent = res.title
+            let heading1 = document.createElement('p')
+            heading1.innerText = res.description
+            let price = document.createElement('p')
+            price.innerText = "$ " + res.price
+            let butt = document.createElement('button')
+            butt.innerText = "Add to Cart"
+            butt.addEventListener('click', () => {
+                srijal(res)
+            })
+
+            card.appendChild(img)
+            card.appendChild(title)
+            card.appendChild(heading1)
+            card.appendChild(price)
+            card.appendChild(butt)
+            card.classList.add("card")
+            con.appendChild(card)
+
+        })
+    }).catch((error) => {
+        console.error('Error:', error)
+    })
+
+let srijal = (res) => {
+    const cartitem = document.createElement('div')
+    const title = document.createElement('p')
+    title.innerText = res.title
+    const delbtn = document.createElement('button')
+    delbtn.innerText = "delete"
+
+    cartitem.appendChild(title)
+    cartitem.appendChild(delbtn)
+    cart.appendChild(cartitem)
+
+    delbtn.addEventListener('click', () => {
+        cartitem.remove()
+    })
+}
